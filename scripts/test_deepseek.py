32f105146fdd81e303d59bc55506870372c34385
Stanley Huang <stanh2019@gmail.com>
2026-09-15 13:06:26 -0500

    Added two scripts, one to explore the gpqa diamond questions and one to make sure that deepseek works

---
+++ scripts/test_deepseek.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.environ["DEEPSEEK_API_KEY"],
+    base_url="https://api.deepseek.com",
+)
+
+response = client.chat.completions.create(
+    model="deepseek-flash",
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False,
+    reasoning_effort="high",
+    extra_body={"thinking": {"type": "enabled"}}
+)
+
+print(response.choices[0].message.content)
+
